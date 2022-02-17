fc08616ec3a54eb8fc34c643eb4ffda1b69725e2
BenTec3d <bentec3d@users.noreply.github.com>
2022-02-17 14:58:00 +0100

    now accepts IEnumerable<string> for sql commands

--- Interface/IDataAccess.cs
+++ Interface/IDataAccess.cs
@@ -7,7 +7,9 @@ namespace DataLibrary
     {
         public ConnectionString Cs { get; set; }
         public Task<List<T>> LoadData<T>(string sql);
+        public Task<List<T>> LoadData<T>(IEnumerable<string> sql);
         public Task SaveData<T>(string sql, T parameters);
+        public Task SaveData<T>(IEnumerable<string> sql, T parameters);
         public void Open();
     }
 }
--- SQLite/DataAccessSQLite.cs
+++ SQLite/DataAccessSQLite.cs
@@ -18,13 +18,29 @@ namespace DataLibrary
             var rows = await connection.QueryAsync<T>(sql);
             return rows.ToList();
         }
-
+        public async Task<List<T>> LoadData<T>(IEnumerable<string> sql)
+        {
+            using IDbConnection connection = new SqliteConnection(Cs.ToString());
+            List<T> result = new();
+            foreach (string s in sql)
+            {
+                result.AddRange((await connection.QueryAsync<T>(s)).ToList());
+            }
+            return result;
+        }
         public Task SaveData<T>(string sql, T parameters)
         {
             using IDbConnection connection = new SqliteConnection(Cs.ToString());
             return connection.ExecuteAsync(sql, parameters);
         }
-
+        public async Task SaveData<T>(IEnumerable<string> sql, T parameters)
+        {
+            using IDbConnection connection = new SqliteConnection(Cs.ToString());
+            foreach (string s in sql)
+            {
+                await connection.ExecuteAsync(s, parameters);
+            }
+        }
         public void Open()
         {
             using IDbConnection connection = new SqliteConnection(Cs.ToString());
